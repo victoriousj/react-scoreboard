ea15f6c19b6ff2310a6f8e49255de945e8bb8eaa
VictoriousJ <victordjohnson@gmail.com>
2018-09-19 18:04:18 -0500

    added the store finally

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import {createStore } from 'redux'
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 
+import PlayerReducer from './src/reducers/players'
 import Scoreboard from './src/containers/Scoreboard';
 
+const store = createStore(
+    PlayerReducer
+);
+
 render (
-    <Scoreboard />,
+    <Provider store={store} >
+        <Scoreboard />
+    </Provider>,
     document.getElementById('root')
 )
--- src/containers/Scoreboard.js
+++ src/containers/Scoreboard.js
@@ -1,83 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
+import * as PlayerActionCreators from '../actions/players';
 import AddPlayerForm from '../components/AddPlayerForm';
 import Header from '../components/Header';
 import Player from '../components/Player';
 
 class Scoreboard extends Component{
-  state = {
-    currentKey: 0,
-    players: [
-      {
-        key: 0,
-        name: 'Jim Hoskins',
-        score: 31,
-      },
-      {
-        key: 1,
-        name: 'Andrew Chalkley',
-        score: 20,
-      },
-      {
-        key: 2,
-        name: 'Alena Holligan',
-        score: 50,
-      },
-    ],
-  };
 
-  getNextKey = () => {
-    let highestKey = this.state.currentKey;
-    this.state.players.forEach(player => {
-      if (player.key > this.state.currentKey) { 
-        highestKey = player.key;
-      }
-    });
-    this.setState({currentKey: ++highestKey});
-    return highestKey;
-  }
-
-  onScoreChange = (index, delta) => {
-    this.state.players[index].score += delta;
-    this.setState(this.state);
-  };
-
-  onAddPlayer = name => {
-    this.state.players.push(
-      { 
-        name: name, 
-        score: 0, 
-        key: this.getNextKey() 
-      });
-    this.setState(this.state);
-  };
-
-  onRemovePlayer = index => {
-    this.state.players.splice(index, 1);
-    this.setState(this.state);
+  static propTypes = {
+    players: PropTypes.array.isRequired,
   };
 
   render() {
+
+    const {dispath, players } = this.props;
+    const addPlayer = bindActionCreators(PlayerActionCreators.addPlayer, dispath);
+    const removePlayer = bindActionCreators(PlayerActionCreators.removePlayer, dispath);;
+    const updatePlayerScore = bindActionCreators(PlayerActionCreators.updatePlayerScore, dispath);;
+
+    const playerComponents = players.map((player, index) => (
+      <Player
+        index={index}
+        key={player.name}
+        name={player.name}
+        score={player.score}
+        removePlayer={removePlayer}
+        updatePlayerScore={updatePlayerScore}
+        />
+    ));
+
     return (
       <div className="scoreboard">
-        <Header players={this.state.players} />
+        <Header players={players} />
         <div className="players">
-          {this.state.players.map(function(player, index) {
-            return (
-              <Player
-                key={player.key}
-                name={player.name}
-                score={player.score}
-                onRemove={() => this.onRemovePlayer(index)}
-                onScoreChange={(delta) => this.onScoreChange(index, delta)}
-              />
-            );
-           }.bind(this))}
+          { playerComponents }
         </div>
-        <AddPlayerForm onAdd={this.onAddPlayer} />
+        <AddPlayerForm addPlayer={addPlayer} />
       </div>
     );
   }
 };
 
-export default Scoreboard;
+mapStateToProps = state => (
+  {
+    players: state
+  }
+);
+
+export default connect(mapStateToProps)(Scoreboard);
