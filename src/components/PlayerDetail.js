602738c3357455796e39e83b47f4921d396a618f
VictoriousJ <victordjohnson@gmail.com>
2018-09-24 21:28:09 -0500

    added prop validation for the player detail component

--- src/components/PlayerDetail.js
+++ src/components/PlayerDetail.js
@@ -1,22 +1,22 @@
 import React, { PropTypes } from 'react';
 
-const PlayerDetail = ({selectedPlayer}) => {
-  if(selectedPlayer){
+const PlayerDetail = (props) => {
+  if(props.selectedPlayer){
     return (
       <div>
-        <h3>{selectedPlayer.name}</h3>
+        <h3>{props.selectedPlayer.name}</h3>
         <ul>
           <li>
             <span>Score: </span> 
-            {selectedPlayer.score}
+            {props.selectedPlayer.score}
           </li>
           <li>
             <span>Created: </span> 
-            {selectedPlayer.created}
+            {props.selectedPlayer.created}
           </li>
           <li>
             <span>Updated: </span> 
-            {selectedPlayer.updated}
+            {props.selectedPlayer.updated}
           </li>        
         </ul>
       </div>
@@ -26,5 +26,14 @@ const PlayerDetail = ({selectedPlayer}) => {
     return (<p>Click on a player to see more details</p>);
   }
 };
+
+PlayerDetail.PropTypes = {
+  selectedPlayer: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    score: PropTypes.number.isRequired,
+    created: PropTypes.instanceOf(Date),
+    updated: PropTypes.instanceOf(Date),
+  }).isRequired
+}
 
 export default PlayerDetail;
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -2,8 +2,8 @@ import React, { PropTypes } from 'react';
 
 const Stats = props => {
   const playerCount = props.players.length;
-  const totalPoints = props.players.reduce((total, player) => 
-    total + player.score, 0);
+  
+  const totalPoints = props.players.reduce((total, player) => total + player.score, 0);
 
   return (
     <table className="stats">
